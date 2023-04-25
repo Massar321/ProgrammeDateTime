ae81df1146422a5f81cfd9d648804f4f939535f0
massar <massar.mhd.ali@gmail.com>
2023-04-25 16:49:34 +0200

    Ajoutez des fichiers projet.

---
+++ ProgrammeDateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgrammeDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            //Console.WriteLine(date.Month);
+
+            CultureInfo cultureFrancais = CultureInfo.GetCultureInfo("fr-FR");
+
+            Console.WriteLine(date.ToString("dddd dd MMMM yyyy HH:mm:ss", cultureFrancais));
+
+            DateTime dateDemain = date.AddDays(1);
+
+            Console.WriteLine("Demain : " + dateDemain.ToString("dddd dd MMMM yyyy HH:mm:ss", cultureFrancais));
+
+            var diff = dateDemain - date;
+            Console.WriteLine("Différence heures : " + diff.TotalHours);
+        }
+    }
+}
